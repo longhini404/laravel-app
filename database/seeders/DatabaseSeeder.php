6ad484dc45eadba3b1a50f62c73324070e61a6a1
guilhermelonghini@gmail.com <guilhermelonghini@gmail.com>
2023-10-11 23:03:31 -0300

    feat: seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Guilherme Longhini',
+            'email' => 'guilhermelonghini@gmail.com',
+            'password' => 'p4ssw0rd',
+        ]);
     }
 }
